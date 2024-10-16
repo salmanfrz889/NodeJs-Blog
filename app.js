c88548300234850665bf7309584032ccb522eed7
salmanul faris <salmanfrz889@gmail.com>
2024-10-16 14:54:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
+
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+
+
+const app = express();
+const PORT = 3001 || process.env.PORT;
+
+
+// Connect to DB
+connectDB();
+
+app.use(express.urlencoded ( { extended: true } ));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session( {
+    secret: 'keyboard cat',
+    resave: 'false',
+    saveUninitialized: true,
+    store: MongoStore.create( {
+        mongoUrl: process.env.MONGODB_URL
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
+
+app.use(express.static('public'));
+
+// Template Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.locals.isActiveRoute = isActiveRoute;
+
+/*app.get('', (req, res) => {
+    res.send("Hello world");
+});*/
+
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
